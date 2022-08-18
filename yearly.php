17d5bb039b241980da0af0c7f2275e3d371305b7
Md. Masum Musfique <66321598+Masumiub@users.noreply.github.com>
2022-08-19 02:39:08 +0600

    Update yearly.php

--- yearly.php
+++ yearly.php
@@ -1,3 +1,30 @@
+<?php
+	/* Database connection settings */
+	$host = 'localhost';
+	$user = 'root';
+	$pass = '';
+	$db = 'testproject';
+	$mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
+
+	$YEAR = '';
+	$AQI = '';
+
+	//query to get data from the table
+	$sql = "SELECT Year, AVG(PM25) FROM `stationwise_data` GROUP BY Year";
+    $result = mysqli_query($mysqli, $sql);
+
+	//loop through the returned data
+	while ($row = mysqli_fetch_array($result)) {
+
+		$AQI = $AQI . '"'. $row['AVG(PM25)'] .'",';
+        $YEAR = $YEAR . '"'. $row['Year'].'",';
+	}
+
+    $AQI = trim($AQI,",");
+	$YEAR = trim($YEAR,",");
+	
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,112 +39,117 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Nosifer&display=swap" rel="stylesheet">
     <link href="http://fonts.cdnfonts.com/css/clash-display" rel="stylesheet">
+    <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
 
-
-
-    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <script type="text/javascript">
-google.charts.load('current', {packages: ['corechart', 'bar']});
-google.charts.setOnLoadCallback(drawBasic);
-
-function drawBasic() {
-
-      var data = new google.visualization.DataTable();
-      data.addColumn('timeofday', 'Time of Day');
-      data.addColumn('number', 'Motivation Level');
-
-      data.addRows([
-        [{v: [8, 0, 0], f: '8 am'}, 1],
-        [{v: [9, 0, 0], f: '9 am'}, 2],
-        [{v: [10, 0, 0], f:'10 am'}, 3],
-        [{v: [11, 0, 0], f: '11 am'}, 4],
-        [{v: [12, 0, 0], f: '12 pm'}, 5],
-        [{v: [13, 0, 0], f: '1 pm'}, 6],
-        [{v: [14, 0, 0], f: '2 pm'}, 7],
-        [{v: [15, 0, 0], f: '3 pm'}, 8],
-        [{v: [16, 0, 0], f: '4 pm'}, 9],
-        [{v: [17, 0, 0], f: '5 pm'}, 10],
-      ]);
-
-      var options = {
-        title: 'Motivation Level Throughout the Day',
-        hAxis: {
-          title: 'Time of Day',
-          format: 'h:mm a',
-          viewWindow: {
-            min: [7, 30, 0],
-            max: [17, 30, 0]
-          }
-        },
-        vAxis: {
-          title: 'Rating (scale of 1-10)'
-        }
-      };
-
-      var chart = new google.visualization.ColumnChart(
-        document.getElementById('chart_div'));
-
-      chart.draw(data, options);
-    }
-    </script>
-
+    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.9.2/html2pdf.bundle.js"></script>
     </head>
 
 <body style="font-family: 'Clash Display', sans-serif;">
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light p-md-3">
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary p-md-3">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">CASE</a>
+    <a class="navbar-brand" href="index.php">CASE MIS</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+          <a class="nav-link" href="seasonwise.php">Seasonwise AQI</a>
         </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
+          
+        <li class="nav-item">
+          <a class="nav-link" href="yearly.php" tabindex="-1" >Yearly AQI</a>
+        </li>
+          
+        <li class="nav-item">
+          <a class="nav-link" href="divisionwise.php" tabindex="-1" >Divisionwise AQI</a>
+        </li>
+          
+        <li class="nav-item">
+          <a class="nav-link" href="stationwise.php" tabindex="-1" >Stationwise AQI</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#" tabindex="-1" >Link</a>
+          <a class="nav-link" href="comparison.php" tabindex="-1" >Compare AQI</a>
         </li>
+          
+        <li class="nav-item">
+          <a class="nav-link" href="login.php" tabindex="-1" >Upload Data</a>
+        </li>
+
+
       </ul>
       <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">SignUp</button>
+
+        <a href="signup.php" class="btn btn-success" tabindex="-1" role="button">SignUp</a>
+          
       </form>
     </div>
   </div>
 </nav>
 
-<div class="container md-3">
+<div class="container md-3" id="all">
         <div class="div text-center">
           <h1 style="padding: 40px;">
              Yearly average AQI data.
           </h1>
-          <button class="btn btn-primary" type="submit">Upload CSV File</button>
-          <button class="btn btn-success" type="submit">Download AQI Report</button>
+          <a href="login.php" class="btn btn-primary" role="button">Upload CSV File</a>
+         <button class="btn btn-success" type="submit" id="download">Download AQI Report</button>
         </div>
         
         <div class="row" style="padding: 40px;">
             <div class="col-md-12 col-lg-12 col-12">
-                <div id="chart_div"></div>
+                <div id="myPlot"></div>
             </div>
+            
+            <script>
+                var xArray = [<?php echo $YEAR; ?>];
+                var yArray = [<?php echo $AQI; ?>];
+
+                var data = [{
+                  x:xArray,
+                  y:yArray,
+                  type:"bar"
+                }];
+
+                var layout = {title:"Yearly Average AQI Data Visualization"};
+
+                Plotly.newPlot("myPlot", data, layout);
+                </script>
         </div> 
 </div>
 
+    
+    <footer class="py-5" style="background-color: #0d6efd; margin-top: 90px;">
+    <div class="container">
+      <h5 class="m-0 text-center text-white">Copyright &copy; CASE MIS - Designed &amp; Developed by Md. Masum Musfique</h5>
+    </div>
+    </footer>
+    
+
+    
+    <script>
+    window.onload = function () {
+    document.getElementById("download")
+        .addEventListener("click", () => {
+            const invoice = this.document.getElementById("myPlot");
+            console.log(invoice);
+            console.log(window);
+            var opt = {
+                margin: 0,
+                filename: 'yearAQIreport.pdf',
+                image: { type: 'jpeg', quality: 0.98 },
+                html2canvas: { scale: 1 },
+                jsPDF: { unit: 'in', format: 'letter', orientation: 'landscape' }
+            };
+            html2pdf().from(invoice).set(opt).save();
+        })
+}
+    </script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
