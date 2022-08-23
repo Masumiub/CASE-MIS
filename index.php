2e818a00b44bc3d6b7de840c750edc33dd92ce9c
Md. Masum Musfique <66321598+Masumiub@users.noreply.github.com>
2022-08-23 23:40:57 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,43 @@
+<?php
+	/* Database connection settings */
+	$host = 'localhost';
+	$user = 'root';
+	$pass = '';
+	$db = 'testproject';
+	$mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
+
+    $Division = '';
+	$Location = '';
+	$AQI = '';
+    $LONGI = '';
+    $LATI = '';
+
+	//query to get data from the table
+	$sql = "SELECT * FROM `mapaqi` ";
+    $result = mysqli_query($mysqli, $sql);
+
+	//loop through the returned data
+	while ($row = mysqli_fetch_array($result)) {
+
+        $Division = $Division . '"'. $row['Division'].'",';
+		$Location = $Location . '"'. $row['Location'].'",';
+		$AQI = $AQI . '"'. $row['AQI'] .'",';
+        
+        $LONGI = $LONGI . '"'. $row['LONGI'] .'",';
+        $LATI = $LATI . '"'. $row['LATI'] .'",';
+	}
+
+    $Division = trim($Division,",");
+	$Location = trim($Location,",");
+	$AQI = trim($AQI,",");
+
+    $LONGI = trim($LONGI,",");
+    $LATI = trim($LATI,",");
+
+    $conn = mysqli_connect("localhost", "root", "", "testproject");
+    
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,11 +52,16 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Nosifer&display=swap" rel="stylesheet">
     <link href="http://fonts.cdnfonts.com/css/clash-display" rel="stylesheet">
+        <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     
-    
+    <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+    />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.4/jspdf.debug.js" ></script>
+    
 </head>
-<body style="font-family: 'Clash Display', sans-serif;">
+<body class="bg-dark text-white" style="font-family: 'Clash Display', sans-serif;">
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary p-md-3">
   <div class="container-fluid">
@@ -48,11 +93,6 @@
           <a class="nav-link" href="comparison.php" tabindex="-1" >Compare AQI</a>
         </li>
           
-        <li class="nav-item">
-          <a class="nav-link" href="login.php" tabindex="-1" >Upload Data</a>
-        </li>
-
-
       </ul>
       <form class="d-flex">
 
@@ -63,9 +103,9 @@
   </div>
 </nav>
     
-    <div class="div" style="background-color: #fafafa; padding: 40px;" id="header">
+    <div class="div animate__animated animate__fadeInDown" style="background-color: #3d3d3d; padding: 40px; box-shadow: rgba(0, 0, 0, 0.65)" id="header" >
         <div class="container">
-            <div class="row d-flex align-items-center">
+            <div class="row d-flex align-items-center" >
                 <div class="col-md-6 col-lg-6 col-12">
                     <h1 style="font-size: 72px;">CASE</h1>
                     <h2>Clean Air & Sustainable Environment</h2>
@@ -87,21 +127,39 @@
           <h1>View Areawise AQI Report</h1>
           <h6>CHOSSE A AREA:  </h6>
               
-<!--            <select name="area" id="area" class="form-select" aria-label="Default select example">-->
-         <select name="area" id="area" style="height: 30px; width: 60%;">
+         <select name="area" id="area" style="height: 30px; width: 60%;" onchange="showArea(this.value)">
+             <option value="">Select a Division:</option>
               <option value="Dhaka" id="Dhaka">Dhaka</option>
+             <option value="Mymensingh" id="Mymensingh">Mymensingh</option>
               <option value="Rajshahi" id="Rajshahi">Rajshahi</option>
-              <option value="Syllhet" id="Syllhet">Syllhet</option>
+              <option value="Sylhet" id="Sylhet">Sylhet</option>
               <option value="Rangpur" id="Rangpur">Rangpur</option>
               <option value="Barishal" id="Barishal">Barishal</option>
               <option value="Khulna" id="Khulna">Khulna</option>
+             <option value="Chittagong" id="Chittagong">Chittagong</option>
             </select>
               
             <button id="click" class="btn btn-primary btn-sm" type="submit">Search</button>
               
             <div id="empty" style="font-size: 72px; color: white; margin-top: 40px; border-radius: 15px;"></div>
-            <div id="empty2" style="font-size: 42px;  margin-top: 20px; border-radius: 15px;"></div>
+            <div id="empty2" style=" margin-top: 20px;font-family: 'Arial'; color: white"></div>
               
+              
+              <script>
+               function showArea(str) {
+                  if (str == "") {
+                    document.getElementById("empty2").innerHTML = "";
+                    return;
+                  }
+                  const xhttp = new XMLHttpRequest();
+                  xhttp.onload = function() {
+                    document.getElementById("empty2").innerHTML = this.responseText;
+                  }
+                  xhttp.open("GET", "getArea.php?q="+str);
+                  xhttp.send();
+                }
+            </script>
+<!--
             <script>
               var clicked = document.getElementById("click");
               clicked.addEventListener("click", function(event){
@@ -166,34 +224,31 @@
               })
                 
               </script>
+-->
         </div>
         </div>
         <div class="row" style="margin-top: 60px;">
             <div class="col-md-3 col-lg-3 col-12">
-                <div class="card text-center">
-                  <div class="card-header">
-                  </div>
+                <div class="card text-center" style="background-color: #3d3d3d; border-radius: 20px; box-shadow: rgba(0, 0, 0, 0.65) 0px 7px 29px 0px; padding: 25px;">
+
                   <div class="card-body">
                       <img src="img/sun.png" style="width: 70px;">
                     <h5 class="card-title">Seasonwise Report</h5>
                     <a href="seasonwise.php" class="btn btn-success btn-sm" role="button">VIEW</a>
                   </div>
-                  <div class="card-footer text-muted">
-                  </div>
+
                 </div>
             </div>    
 
             <div class="col-md-3 col-lg-3 col-12">
-                <div class="card text-center">
-                  <div class="card-header">
-                  </div>
+                <div class="card text-center" style="background-color: #3d3d3d; border-radius: 20px; box-shadow: rgba(0, 0, 0, 0.65) 0px 7px 29px 0px; padding: 25px;">
+
                   <div class="card-body">
                       <img src="img/cloudy-day.png" style="width: 70px;">
-                    <h5 class="card-title">Yearwise Report</h5>
+                    <h5 class="card-title">Yearwise <br> Report</h5>
                     <a href="yearly.php" class="btn btn-success btn-sm" role="button">VIEW</a>
                   </div>
-                  <div class="card-footer text-muted">
-                  </div>
+
                 </div>
             </div>  
 <!--
@@ -203,16 +258,14 @@
             </div>
 -->
             <div class="col-md-3 col-lg-3 col-12">
-                <div class="card text-center">
-                  <div class="card-header">
-                  </div>
+                <div class="card text-center" style="background-color: #3d3d3d; border-radius: 20px; box-shadow: rgba(0, 0, 0, 0.65) 0px 7px 29px 0px; padding: 25px;">
+
                   <div class="card-body">
                       <img src="img/hot.png" style="width: 70px;">
                     <h5 class="card-title">Divisionwise Report</h5>
                     <a href="divisionwise.php" class="btn btn-success btn-sm" role="button">VIEW</a>
                   </div>
-                  <div class="card-footer text-muted">
-                  </div>
+
                 </div>
             </div>  
             
@@ -224,16 +277,14 @@
 -->
             
             <div class="col-md-3 col-lg-3 col-12">
-                <div class="card text-center">
-                  <div class="card-header">
-                  </div>
+                <div class="card text-center" style="background-color: #3d3d3d; border-radius: 20px; box-shadow: rgba(0, 0, 0, 0.65) 0px 7px 29px 0px; padding: 25px;">
+
                   <div class="card-body">
                     <img src="img/storm.png" style="width: 70px;">
                     <h5 class="card-title">Stationwise Report</h5>
                     <a href="stationwise.php" class="btn btn-success btn-sm" role="button">VIEW</a>
                   </div>
-                  <div class="card-footer text-muted">
-                  </div>
+
                 </div>
             </div>  
 <!--
@@ -246,35 +297,33 @@
         
         <div class="row" style="margin-top: 30px;">
             <div class="col-md-6 col-lg-6 col-12">
-                <div class="card text-center">
-                  <div class="card-header">
-                  </div>
+                <div class="card text-center" style="background-color: #3d3d3d; border-radius: 20px; box-shadow: rgba(0, 0, 0, 0.65) 0px 7px 29px 0px; padding: 25px;">
+
                   <div class="card-body">
                       <img src="img/rainy-day.png" style="width: 90px;">
                     <h5 class="card-title">Compare AQIs of Data Sources</h5>
                     <a href="comparison.php" class="btn btn-success btn-sm" role="button">VIEW</a>
                   </div>
-                  <div class="card-footer text-muted">
-                  </div>
+                    
                 </div>
             </div>  
             
             <div class="col-md-6 col-lg-6 col-12">
-                <div class="card text-center">
-                  <div class="card-header">
-                  </div>
+                <div class="card text-center" style="background-color: #3d3d3d; border-radius: 20px; box-shadow: rgba(0, 0, 0, 0.65) 0px 7px 29px 0px; padding: 25px;">
+
                   <div class="card-body">
                       <img src="img/cloudy.png" style="width: 90px;">
                     <h5 class="card-title">Overall Bangladesh AQI Report</h5>
                     <a href="map.php" class="btn btn-success btn-sm" role="button">VIEW</a>
                   </div>
-                  <div class="card-footer text-muted">
-                  </div>
+
                 </div>
             </div>
         </div>
     </div>
     
+    
+
     <footer class="py-5" style="background-color: #0d6efd; margin-top: 90px;">
     <div class="container">
       <h5 class="m-0 text-center text-white">Copyright &copy; CASE MIS - Designed &amp; Developed by Md. Masum Musfique</h5>
@@ -282,23 +331,7 @@
   </footer>
     
     
-<!--
-     <script>
-    function generatePDF() {
-         var doc = new jsPDF();
-          doc.fromHTML(document.getElementById("header"), // page element which you want to print as PDF
-          15,
-          15, 
-          {
-            'width': 170
-          },
-          function(a) 
-           {
-            doc.save("OverallBangladeshMapAqI.pdf");
-          });
-        }
-</script>   
--->
+
     
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
